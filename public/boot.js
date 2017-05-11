4b90c1cc87b7542b399ba1a08c3c0b0a4df20ac3
ttymed <tyler.boright@gmail.com>
2017-05-11 18:44:39 +0800

    removes any preloading code and just allows for the boot screen to run the Binary state to work on the binary puzzle

--- public/boot.js
+++ public/boot.js
@@ -4,7 +4,9 @@ Hacked.Boot = function (game) {}
 
 Hacked.Boot.prototype = {
 	init: function() {},
-	preload: function() {},
+	preload: function() {
+		// can put the loading bar in here
+	},
 	create: function() {
 		this.game.state.start('Binary');
 	}
