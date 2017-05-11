5b023c9bb43b57e4519e48d48341684c25fccf4d
ttymed <tyler.boright@gmail.com>
2017-05-11 18:43:57 +0800

    adds code to allow the gem sprite to render to the screen and move

--- public/binaryPuzzle.js
+++ public/binaryPuzzle.js
@@ -6,13 +6,28 @@ Hacked.Binary = function(game) {
 
 Hacked.Binary.prototype = {
 	preload: function() {
-		this.game.load.image('logo', 'phaser.png');
+		this.game.load.image('gem', 'gem.png')
 	},
 	create: function() {
-		var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-		logo.anchor.setTo(0.5, 0.5);
+		this.gem = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'gem');
+		this.game.physics.startSystem(Phaser.Physics.P2JS);
+
+
+		this.game.physics.p2.enable(this.gem);
+
+		this.cursors = this.game.input.keyboard.createCursorKeys();
 	},
 	update: function() {
+		this.gem.body.setZeroVelocity();
 
+		if (this.cursors.left.isDown) {
+			this.gem.body.moveLeft(200);
+		} else if (this.cursors.right.isDown) {
+			this.gem.body.moveRight(200);
+		} else if (this.cursors.up.isDown) {
+			this.gem.body.moveUp(200);
+		} else if (this.cursors.down.isDown) {
+			this.gem.body.moveDown(200);
+		}
 	},
 };
