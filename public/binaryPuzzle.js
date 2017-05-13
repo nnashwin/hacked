1032d55663a3ca9df4f573b2dc887b5dffac2f90
ttymed <tyler.boright@gmail.com>
2017-05-13 15:45:40 +0800

    adds a second gem that can not move

--- public/binaryPuzzle.js
+++ public/binaryPuzzle.js
@@ -11,17 +11,27 @@ Hacked.Binary.prototype = {
 	},
 	create: function() {
 		this.player = Hacked.addSprite(this.game, 'alucard', this.game.world.centerX, this.game.world.centerY);
-		this.gem = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'gem');
+		this.gem = this.game.add.sprite(200, this.game.world.centerY, 'gem');
+		this.gem2 = this.game.add.sprite(100, this.game.world.centerY, 'gem');
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
 
-		Hacked.addArcadePhysicsToSprite(this.gem);
-		Hacked.addArcadePhysicsToSprite(this.player);
+
+		Hacked.addArcadePhysicsToSprite([this.gem, this.player, this.gem2]);
+		this.gem2.body.immovable = true;
+
 		this.cursors = this.game.input.keyboard.createCursorKeys();
 	},
 	update: function() {
 		this.player.body.velocity.x = 0;
 		this.player.body.velocity.y = 0;
+
+		this.gem.body.velocity.x = 0;
+		this.gem.body.velocity.y = 0;
+
+		this.gem2.body.velocity.x = 0;
+		this.gem2.body.velocity.y = 0;
+
 		if (this.cursors.left.isDown) {
 			this.player.body.velocity.x = -150;
 		} else if (this.cursors.right.isDown) {
@@ -31,5 +41,17 @@ Hacked.Binary.prototype = {
 		} else if (this.cursors.down.isDown) {
 			this.player.body.velocity.y = 150;
 		}
+
+		this.game.physics.arcade.collide(this.player, this.gem, this.collide, null, this);
+		this.game.physics.arcade.collide(this.player, this.gem2, this.collide, null, this);
 	},
+
+	collide (playerObj, collisionObj) {
+		this.game.stage.backgroundColor = '#992d2d';
+	},
+
+	render () {
+		this.game.debug.body(this.player);
+		this.game.debug.body(this.gem);
+	}
 };
