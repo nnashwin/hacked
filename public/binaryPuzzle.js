cf6944b253066ea2bdc4469ee66cb34fe101856a
ttymed <tyler.boright@gmail.com>
2017-05-16 16:48:59 +0800

    adds an overlap function to send alucard to the center of the landing when it occurs.

--- public/binaryPuzzle.js
+++ public/binaryPuzzle.js
@@ -13,25 +13,19 @@ Hacked.Binary.prototype = {
 	create: function() {
 
 		this.group = this.game.add.group();
+		this.gems = this.game.add.group();
 
 		this.player = this.group.create(300, 28, 'alucard');
-		this.gem = this.group.create(200, this.game.world.centerY, 'gem');
-		this.gem2 = this.game.add.sprite(100, this.game.world.centerY, 'gem');
+		this.gem2 = this.gems.create(100, this.game.world.centerY, 'gem');
 		this.landing = this.group.create(300, 400, 'landing');
 
-
-
-		Hacked.addArcadePhysicsToSprite([this.gem, this.player, this.gem2]);
-		this.gem2.body.immovable = true;
+		Hacked.addArcadePhysicsToSprite([this.player, this.gem2, this.landing]);
 
 		this.cursors = this.game.input.keyboard.createCursorKeys();
 	},
 	update: function() {
 		this.player.body.velocity.x = 0;
 		this.player.body.velocity.y = 0;
-
-		this.gem.body.velocity.x = 0;
-		this.gem.body.velocity.y = 0;
 
 		this.gem2.body.velocity.x = 0;
 		this.gem2.body.velocity.y = 0;
@@ -46,18 +40,35 @@ Hacked.Binary.prototype = {
 			this.player.body.velocity.y = 150;
 		}
 
-		this.game.physics.arcade.collide(this.player, this.gem, this.collide, null, this);
 		this.game.physics.arcade.collide(this.player, this.gem2, this.collide, null, this);
 
 		this.group.sort('y', Phaser.Group.SORT_ASCENDING);
+
+		if (this.checkOverlap(this.gem2, this.landing) && !this.gem2.overlapping) {
+			this.gem2.destroy();
+			this.gem2 = this.gems.create(350, 450, 'alucard');
+
+			Hacked.addArcadePhysicsToSprite([this.gem2]);
+			this.game.physics.arcade.collide(this.player, this.gem2, this.collide, null, this);
+			this.gem2.overlapping = !this.gem2.overlapping;
+		} else if (this.checkOverlap(this.gem2, this.landing) === false) {
+			this.gem2.overlapping = false;
+		}
 	},
 
 	collide (playerObj, collisionObj) {
 		this.game.stage.backgroundColor = '#992d2d';
 	},
 
+	checkOverlap (spriteA, spriteB) {
+		let boundsA = spriteA.getBounds();
+		let boundsB = spriteB.getBounds();
+
+		return Phaser.Rectangle.intersects(boundsA, boundsB);
+	},
+
 	render () {
 		this.game.debug.body(this.player);
-		this.game.debug.body(this.gem);
+		this.game.debug.body(this.gem2);
 	}
 };
