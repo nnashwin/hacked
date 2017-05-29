340c003dc589e1af73cb0f0615b96d283a67e266
ttymed <tyler.boright@gmail.com>
2017-05-29 16:42:09 +0800

    binary blocks which are on the landing will now change to opposite when the opposite picture is stepped on

--- public/binaryPuzzle.js
+++ public/binaryPuzzle.js
@@ -6,42 +6,39 @@ Hacked.Binary = function(game) {
 
 Hacked.Binary.prototype = {
 	preload: function() {
-		this.game.load.image('gem', 'gem.png');
 		this.game.load.image('alucard', 'alucard-sprite.png');
 		this.game.load.image('binary-0', 'binary-0.png');
 		this.game.load.image('binary-1', 'binary-1.png');
 		this.game.load.image('landing', 'landing-sprite.jpeg');
+		this.game.load.image('opposite', 'opposite.jpeg');
 	},
 	create: function() {
 
 		this.group = this.game.add.group();
-		this.gems = this.game.add.group();
 		this.binaries = this.game.add.group();
 
 		Hacked.Binary.addBinaryToGroup(this.binaries, 200, 10, 0);
 		Hacked.Binary.addBinaryToGroup(this.binaries, 400, 10, 1);
 		Hacked.Binary.addBinaryToGroup(this.binaries, 400, 300, 1);
 		Hacked.Binary.addBinaryToGroup(this.binaries, 200, 300, 0);
+		Hacked.Binary.addBinaryToGroup(this.binaries, 200, 500, 1);
+		Hacked.Binary.addBinaryToGroup(this.binaries, 200, 800, 0);
 
 		this.binaries.children.map((binary) => {
-			console.log(binary);
 			Hacked.addArcadePhysicsToSprite(binary);
 		});
 
 		this.player = this.group.create(300, 28, 'alucard');
-		this.gem2 = this.gems.create(100, this.game.world.centerY, 'gem');
 		this.landing = this.group.create(300, 400, 'landing');
+		this.opposite = this.group.create(0, 0, 'opposite');
 
-		Hacked.addArcadePhysicsToSprite([this.player, this.gem2, this.landing]);
+		Hacked.addArcadePhysicsToSprite([this.player, this.landing]);
 
 		this.cursors = this.game.input.keyboard.createCursorKeys();
 	},
 	update: function() {
 		this.player.body.velocity.x = 0;
 		this.player.body.velocity.y = 0;
-
-		this.gem2.body.velocity.x = 0;
-		this.gem2.body.velocity.y = 0;
 
 		if (this.cursors.left.isDown) {
 			this.player.body.velocity.x = -150;
@@ -68,8 +65,6 @@ Hacked.Binary.prototype = {
 			}
 		}
 
-		this.game.physics.arcade.collide(this.player, this.gem2, this.collide, null, this);
-
 		this.group.sort('y', Phaser.Group.SORT_ASCENDING);
 
 		if (Hacked.checkOverlap(this.player, this.landing) && !this.player.overlapping) {
@@ -82,6 +77,17 @@ Hacked.Binary.prototype = {
 		} else if (Hacked.checkOverlap(this.player, this.landing) === false){
 			this.player.overlapping = false;
 		}
+
+		if (Hacked.checkOverlap(this.opposite, this.player) && !this.player.overlapOpposite) {
+			this.player.overlapOpposite = true;
+			const overlapBinArray = Hacked.Binary.checkOverlappingBinaryBlocks(this.binaries.children, this.landing);
+			overlapBinArray.map((binSprite) => {
+				binSprite.binaryVal === '0' ? binSprite.binaryVal = '1' : binSprite.binaryVal = '0';
+				binSprite.loadTexture(`binary-${binSprite.binaryVal}`, 0);
+			});
+		} else if (!Hacked.checkOverlap(this.opposite, this.player)) {
+			this.player.overlapOpposite = false;
+		}
 	},
 
 	collide (playerObj, collisionObj) {
@@ -90,6 +96,5 @@ Hacked.Binary.prototype = {
 
 	render () {
 		this.game.debug.body(this.player);
-		this.game.debug.body(this.gem2);
 	}
 };
