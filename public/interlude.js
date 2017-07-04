976f023193687361c7603483a4e1b034354619d4
ttymed <tyler.boright@gmail.com>
2017-07-04 14:02:02 -0700

    creates the structure for the TextInterlude state

--- public/interlude.js
+++ public/interlude.js
@@ -5,5 +5,19 @@ Hacked.TextInterlude = function(game) {
 }
 
 Hacked.TextInterlude.prototype = {
+	preload: function () {
 
+	},
+
+	create: function () {
+		console.log('in interlude state');
+	},
+
+	update: function () {
+	
+	},
+
+	render: function () {
+
+	},
 }
