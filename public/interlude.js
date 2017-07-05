293d56f88d9bd9ede383bb6a904f887e2e84247c
ttymed <tyler.boright@gmail.com>
2017-07-05 12:39:15 -0700

    creates a hacky text writing state which will type in text word by word onto the screen.

--- public/interlude.js
+++ public/interlude.js
@@ -10,16 +10,49 @@ Hacked.TextInterlude.prototype = {
 
 	create: function () {
 		const currSceneIdx = this.game.sceneOrder[this.game.currSceneCounter];
-		const dialogueJson = this.game.cache.getJSON('windowDiag')[currSceneIdx];
-		console.log(dialogueJson);
+		this.dialogueContent = this.game.cache.getJSON('windowDiag')[currSceneIdx];
+
+		this.wordIndex = 0;
+		this.lineIndex = 0;
+
+		var style = { font: "15px Arial", fill: "#19de65" };
+		this.winText = this.game.add.text(32, 32, '', { font: "15px Arial", fill: "#19de65", align: "left", boundsAlignH: "top", boundsAlignV:"top" });
+		this.nextLine();
+
 	},
 
-	update: function () {
-	
+	nextLine: function () {
+		const wordDelay = 120;
+		const lineDelay = 400;
+		let line = [];
+		
+		
+		if (this.lineIndex === this.dialogueContent.length) {
+			console.log('finished');
+			return;
+		}
+
+		line = this.dialogueContent[this.lineIndex].split(" ");
+
+		this.wordIndex = 0;
+
+		this.game.time.events.repeat(wordDelay, line.length, this.nextWord, this, [line, lineDelay]);
+
+		this.lineIndex++;
+
 	},
 
-	render: function () {
+	nextWord: function (textArgs) {
+		let [line, lineDelay] = textArgs;
+		this.winText.text = this.winText.text.concat(line[this.wordIndex] + " ");
 
-	},
+		this.wordIndex++;
 
+		if (this.wordIndex === line.length) {
+			this.winText.text = this.winText.text.concat("\n");
+
+			this.game.time.events.add(lineDelay, this.nextLine, this);
+		}
+		
+	}
 }
